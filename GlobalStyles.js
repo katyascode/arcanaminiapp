50948dacdb2c4633a804ec9283129d9bec8f5a5e
jaspergass <115849451+jaspergass@users.noreply.github.com>
2023-12-21 21:38:18 -0800

    Global Styles + Assets (fonts, imgs)

---
+++ GlobalStyles.js
@@ -0,0 +1,47 @@
+/* fonts */
+export const FontFamily = {
+    nunitoRegular: "Nunito-Regular",
+    aoboshiOneRegular: "AoboshiOne-Regular",
+    nunitoSemiBold: "Nunito-SemiBold",
+    germaniaOneRegular: "GermaniaOne-Regular"
+};
+
+/* Colors */
+export const Color = {
+    white: "#fff",
+    black: "#423d38",
+    grey80: "#6A6764",
+    grey70: "#7A7775",
+    grey50: "#CBCBCB",
+    grey30: "#D9D9D9",
+    grey20: "#E6E6E6",
+    grey10: "#F1F1F1",
+    purple: "#7A86ED"
+};
+
+/* font sizes */
+export const FontSize = {
+    size_sm: 14,
+    size_lg: 18,
+    size_xxl: 26,
+    size_base: 16,
+    size_xl: 20,
+    size_xs: 12,
+    size_max: 100
+};
+
+/* Paddings */
+export const Padding = {
+    p_60: 60,
+    p_12: 12,
+    p_36: 36,
+    p_16: 16,
+    p_32: 32,
+    p_10: 10,
+    p_6: 6,
+};
+
+/* border radiuses */
+export const Border = {
+    br_xl: 20,
+  };
