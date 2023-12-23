acef5f82b7eee39aa1a0abdba996dbb399d31bae
Alex Cai <aca277@sfu.ca>
2023-12-23 14:10:27 -0800

    begin card list formatting

--- app/librarium.js
+++ app/librarium.js
@@ -1,13 +1,51 @@
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text, ScrollView, Image } from 'react-native';
+
+// temporary array to show result of mapping, later will use the data retrieved from card database
+const testArray = [1, 2, 3, 4, 5, 6];
 
 const Librarium = () => {
     return (
-        <View>
-            <Text>
-                Librarium
-            </Text>
-        </View>
+        <ScrollView>
+            <View style={styles.container}>
+                {testArray.map((card) => {
+                    return (
+                        <View style={styles.card}>
+                            <Image style={styles.cardImage} source='https://www.fertilome.com/media/klowrey/Article%20Images/Tree.jpg' />
+                            <Text>{card}</Text>
+                        </View>
+                    )
+                })}
+            </View>
+        </ScrollView>
     )
 }
 
 export default Librarium;
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: '2%',
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    card: {
+        backgroundColor: '#fff',
+        marginBottom: 10,
+        marginLeft: '2%',
+        width: '45%',
+        borderRadius: '15%',
+        alignItems: 'center',
+        padding: '1%'
+    },
+
+    cardImage: {
+        justifyContent: 'center',
+        width: '80%',
+        height: 400,
+        objectFit: 'cover'
+    }
+})
