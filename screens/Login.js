6d9c84b22aef8815a438e7886a5213baf2538b62
GurkiratSinghArora <sgurkirat868@gmail.com>
2024-02-22 01:00:27 -0800

    Fixed the UI of Login Page

--- screens/Login.js
+++ screens/Login.js
@@ -1,4 +1,4 @@
-import { ActivityIndicator, StyleSheet, TextInput,Button, View } from "react-native";
+import { ActivityIndicator, StyleSheet, TextInput,Button, View, KeyboardAvoidingView,Text } from "react-native";
 import { FIREBASE_AUTH } from "../FirebaseConfig";
 import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
@@ -15,22 +15,33 @@ function Login({navigation}){
             const response = await signInWithEmailAndPassword(auth,email, password);
             
         }catch(error){
-            console.log(error);
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+
         }
         setLoading(false);
     }
-
-
-
     return (
-        <View style={styles.container}>
-            <TextInput style={styles.input} placeholderTextColor="#524b4a"  value={email} placeholder="Email" autoCapitalize="none" onChangeText={(text)=>setEmail(text)}></TextInput>
-            <TextInput style={styles.input}  placeholderTextColor="#524b4a" secureTextEntry={true} value={password} placeholder="Password" autoCapitalize="none" onChangeText={(text)=>setPassword(text)}></TextInput>
-            {loading ? (<ActivityIndicator size="large" color="#0000ff" />) : (<>
-                <Button title="Login" onPress={login}/>
-                <Button title="Create an Account" onPress={()=>navigation.navigate('Register')}/>
-                </>)}
-        </View>
+        <KeyboardAvoidingView style={styles.container} behavior="padding"> 
+        <View>
+            <View style={styles.headingView}>
+                <Text style={styles.heading}>Account details</Text>
+            </View>
+            <View style={styles.wrapper}>
+                <Text style={styles.subHeading}>Email</Text>
+                <TextInput style={styles.input} placeholderTextColor="#524b4a"  value={email} placeholder="Enter your email here" autoCapitalize="none" onChangeText={(text)=>setEmail(text)}></TextInput>
+            </View>
+            <View style={styles.wrapper}>
+                <Text style={styles.subHeading}>Password must be 6-12 characters long</Text>
+                <TextInput style={styles.input}  placeholderTextColor="#524b4a" secureTextEntry={true} value={password} placeholder="XXX" autoCapitalize="none" onChangeText={(text)=>setPassword(text)}></TextInput>
+            </View>
+            {loading ? (<ActivityIndicator size="large" color="#0000ff" />) : (<View style={styles.buttonContainer}>
+            <Button title="Login" onPress={login}/>
+            <Button title="Create an account" onPress={()=>navigation.navigate('Register')}/>
+            </View>)}
+     </View>
+        </KeyboardAvoidingView>
     )
 }
 
@@ -40,15 +51,48 @@ const styles = StyleSheet.create({
     container:{
         marginHorizontal: 20,
         flex:1,
-        justifyContent: 'center'
+        justifyContent:"center"
     },
     input:{
         marginVertical: 4,
-        height: 50,
-        borderWidth: 1,
-        borderRadius: 5,
-        padding: 10,
+        height: 48,
+        border: 0,
+        paddingTop: 0,
+        padingBottom:0,
+        paddingLeft: 26,
+        margin:0,
+        fontSize:"17px",
+        
         backgroundColor: '#fff'
-
+    },
+    headingView:{
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    heading:{
+        fontSize: 30,
+        fontWeight: '300',
+        fontWeight:"400",
+        color: "black"
+    },
+    wrapper:{
+        backgroundColor:"white",
+        borderRadius: 20,
+        marginBottom: 10,
+        overflow:"hidden"
+    },
+    subHeading:{
+       color:"#575454",
+       fontSize: 16,
+       fontWeight: "400", 
+       paddingLeft: 20,
+       paddingTop: 8,
+       paddingBottom: 0,
+       marginTop: 5,
+    },
+    buttonContainer:{
+        display:'flex',
+        flexDirection: "row", 
+        justifyContent: "space-evenly",
     }
 })
